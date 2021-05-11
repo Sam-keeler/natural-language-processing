5bdaea0eaa21fba0f22e264ee165daa01eafe08f
Sam Keeler <keelersam@gmail.com>
2021-05-10 22:24:13 -0500

    project

--- acquire.py
+++ acquire.py
@@ -4,6 +4,11 @@ import pandas as pd
 import re
 from requests import get
 from bs4 import BeautifulSoup
+from typing import Dict, List, Optional, Union, cast
+import os
+import json
+from env import github_token, github_username
+headers = {"Authorization": f"token {github_token}", "User-Agent": github_username}
 
 '''
 Takes a url from blog posts on the codeup website and returns the title, date, and content
@@ -77,4 +82,462 @@ def get_all_inshorts(category_list):
         all_inshorts = all_inshorts + all_category_articles
     
     df = pd.DataFrame(all_inshorts)
+    return df
+
+def github_api_request(url: str) -> Union[List, Dict]:
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+    if response.status_code != 200:
+        raise Exception(
+            f"Error response from github api! status code: {response.status_code}, "
+            f"response: {json.dumps(response_data)}"
+        )
+    return response_data
+
+def get_repo_language(repo: str) -> str:
+    url = f"https://api.github.com/repos/{repo}"
+    repo_info = github_api_request(url)
+    if type(repo_info) is dict:
+        repo_info = cast(Dict, repo_info)
+        if "language" not in repo_info:
+            raise Exception(
+                "'language' key not round in response\n{}".format(json.dumps(repo_info))
+            )
+        return repo_info["language"]
+    raise Exception(
+        f"Expecting a dictionary response from {url}, instead got {json.dumps(repo_info)}"
+    )
+
+def get_repo_contents(repo: str) -> List[Dict[str, str]]:
+    url = f"https://api.github.com/repos/{repo}/contents/"
+    contents = github_api_request(url)
+    if type(contents) is list:
+        contents = cast(List, contents)
+        return contents
+    raise Exception(
+        f"Expecting a list response from {url}, instead got {json.dumps(contents)}"
+    )
+
+def get_readme_download_url(files: List[Dict[str, str]]) -> str:
+    """
+    Takes in a response from the github api that lists the files in a repo and
+    returns the url that can be used to download the repo's README file.
+    """
+    for file in files:
+        if file["name"].lower().startswith("readme"):
+            return file["download_url"]
+    return ""
+
+def process_repo(repo: str) -> Dict[str, str]:
+    """
+    Takes a repo name like "gocodeup/codeup-setup-script" and returns a
+    dictionary with the language of the repo and the readme contents.
+    """
+    contents = get_repo_contents(repo)
+    readme_download_url = get_readme_download_url(contents)
+    if readme_download_url == "":
+        readme_contents = ""
+    else:
+        readme_contents = requests.get(readme_download_url).text
+    return {
+        "repo": repo,
+        "language": get_repo_language(repo),
+        "readme_contents": readme_contents,
+    }
+
+def get_the_repos(url):
+    response = requests.get(url)
+    html = response.text
+    soup = bs4.BeautifulSoup(html)
+    bleh = soup.find(class_="Link--muted")['href']
+    bleh = soup.find_all(class_="Link--muted")
+    hold = []
+    for item in bleh:
+        hold.append(item['href'])
+    regexp = r'\/.*\/.*?'
+    lister = []
+    for item in hold:
+        lister.append(re.findall(regexp, item))
+    for i in range(0, len(lister)):
+        lister[i] = str(lister[i])
+        lister[i] = lister[i][3:-3]
+    return lister
+
+def get_all_repos(orig_url):
+    split_url = orig_url.split('?')
+    hold_urls = []
+    all_repos = []
+    for i in range(2, 21):
+        hold_urls.append(split_url[0] + '?p=' + str(i) + '&' + split_url[1])
+    for item in hold_urls:
+        all_repos += get_the_repos(item)
+    return all_repos
+
+def scrape_github_data() -> List[Dict[str, str]]:
+    """
+    Loop through all of the repos and process them. Returns the processed data.
+    """
+    REPOS = ['zq99299/repository-summary',
+ 'CloudCompare/CloudCompare',
+ 'vispy/vispy',
+ 'mootools/mootools-core',
+ 'CodeMazeBlog/async-repository-dotnetcore-webapi',
+ 'AnemoneIndicum/taotao-repository',
+ 'gentoo/gentoo',
+ 'openstack/openstack',
+ 'scummvm/scummvm',
+ 'webpack/webpack.js.org',
+ 'magnayn/Jenkins-Repository',
+ 'WPO-Foundation/webpagetest',
+ 'octocat/Hello-World',
+ 'Torann/laravel-repository',
+ 'maduce/fosscad-repo',
+ 'angular/material-start',
+ 'a34546/Wei.Repository',
+ 'kaiyuanshe/repository',
+ 'wordpress-mobile/WordPress-iOS',
+ 'transmission/transmission',
+ 'slorber/gcm-server-repository',
+ 'tegon/clone-org-repos',
+ 'geometer/FBReaderJ',
+ 'Sage-Bionetworks/Synapse-Repository-Services',
+ 'jenkinsci/repository-connector-plugin',
+ 'zach-morris/repository.zachmorris',
+ 'geotools/geotools',
+ 'libjpeg-turbo/libjpeg-turbo',
+ 'HandBrake/HandBrake',
+ 'Auxilus/termux-x-repository',
+ 'toltec-dev/toltec',
+ 'HIGHWAY99/repository.thehighway',
+ 'esendir/MongoRepository',
+ 'prof-membrane/repository.membrane',
+ 'DSpace/xoai',
+ 'xiaoxiaoqingyi/mine-android-repository',
+ 'Azure-Samples/PartitionedRepository',
+ 'adlerpagliarini/RepositoryPattern-Dapper-EFCore',
+ 'Yara-Rules/rules',
+ 'racket/racket',
+ 'oamg/leapp-repository',
+ 'xmake-io/xmake-repo',
+ 'ErickWendel/generic-repository-nodejs-typescript-article',
+ 'cpina/github-action-push-to-another-repository',
+ 'rinvex/laravel-repositories',
+ 'openvinotoolkit/openvino',
+ 'ga4gh/data-repository-service-schemas',
+ 'popomore/projj',
+ 'ezsystems/repository-forms',
+ 'AllenDowney/ThinkBayes',
+ 'GoogleCloudPlatform/repository-gardener',
+ 'rlidwka/sinopia',
+ 'visminer/repositoryminer',
+ 'ecomfe/spec',
+ 'CastagnaIT/repository.castagnait',
+ 'xuezhijian/Spring-Demo-Repository',
+ 'yona-projects/yona',
+ 'packagemgmt/repositorytools',
+ 'Antergos/antergos-packages',
+ 'udacity/Sunshine-Version-2',
+ 'opencv/opencv_contrib',
+ 'offensive-security/exploitdb',
+ 'XvBMC/repository.xvbmc',
+ 'kisslinux/repo',
+ 'XiumingLee/MingRepository',
+ 'Kitware/CMake',
+ 'helm/chart-releaser',
+ 'pixta-dev/repository-mirroring-action',
+ 'doxygen/doxygen',
+ 'openscenegraph/OpenSceneGraph',
+ 'RedHatTraining/DO180-apps',
+ 'Rarst/release-belt',
+ 'IonicaBizau/repository-downloader',
+ 'layerfMRI/repository',
+ 'EgeBalci/Mass-Hacker-Arsenal',
+ 'rmccue/test-repository',
+ 'renfeng/android-repository',
+ 'aircrack-ng/aircrack-ng-archive',
+ 'pfsense/pfsense',
+ 'microsoft/WPF-Samples',
+ 'twosigma/git-meta',
+ 'winneon/tutorial-repository',
+ 'kodibae/repository.kodibae',
+ 'besnik/generic-repository',
+ 'ethereum/EIPs',
+ 'gperftools/gperftools',
+ 'samvera/active_fedora',
+ 'geoserver/geoserver',
+ 'zhaohehe/z-repository',
+ 'arch4edu/arch4edu',
+ 'simpligility/maven-repository-tools',
+ 'psalterio/repository',
+ 'cheeaun/repokemon',
+ 'I-Hope-Peace/ChangeDetectionRepository',
+ 'covenantkodi/repository.colossus',
+ 'DefinitelyTyped/DefinitelyTyped',
+ 'eddelbuettel/drat',
+ 'repology/repology-rules',
+ 'rdpeng/ProgrammingAssignment2',
+ 'glarizza/puppet_repository',
+ 'mateodelnorte/sourced-repo-mongo',
+ 'tesseract-ocr/tesseract',
+ 'botallen/repository.botallen',
+ 'sonatype-nexus-community/nexus-repository-composer',
+ 'recca0120/laravel-repository',
+ 'develar/settings-repository',
+ 'caffeinated/repository',
+ 'archlinuxcn/mirrorlist-repo',
+ 'sonatype-nexus-community/nexus-repository-apt',
+ 'dotnet/docs',
+ 'dcmi/repository',
+ 'dvorka/mindforger-repository',
+ 'sonatype-nexus-community/nexus-repository-helm',
+ 'renaudcerrato/appengine-maven-repository',
+ 'dotnet/core',
+ 'mateodelnorte/meta',
+ 'zendframework/zendframework',
+ 'mesosphere/universe',
+ 'kodi-community-addons/repository.marcelveldt',
+ 'sonatype-nexus-community/nexus-repository-import-scripts',
+ 'leonvanbokhorst/RepositoryPatternEntityFramework',
+ 'packal/repository',
+ 'jdf76/repository',
+ 'hacker-walker/repository',
+ 'drinfernoo/repository.example',
+ 'AdiChat/Repository-Hunter',
+ 'dobbelina/repository.dobbelina',
+ 'tugberkugurlu/GenericRepository',
+ 'alexandre-spieser/mongodb-generic-repository',
+ 'Guilouz/repository.guilouz',
+ 'jitsi/jitsi-maven-repository',
+ 'freeall/create-repository',
+ 'acoomans/ACCodeSnippetRepositoryPlugin',
+ 'singhsanket143/CppCompetitiveRepository',
+ 'omarabid/Self-Hosted-WordPress-Plugin-repository',
+ 'Vashiel/repository.adulthideout',
+ 'notepad-plus-plus/notepad-plus-plus',
+ 'crcms/repository',
+ 'scipy/scipy',
+ 'LeonWuV/FE-blog-repository',
+ 'DSpace/DSpace',
+ 'peter-evans/repository-dispatch',
+ 'JustryDeng/CommonRepository',
+ 'kisslinux/community',
+ 'Alfresco/alfresco-repository',
+ 'nasa/Common-Metadata-Repository',
+ 'vim/vim',
+ 'brunohbrito/MongoDB-RepositoryUoWPatterns',
+ 'ulyaoth/repository',
+ 'ziglibs/repository',
+ 'megamit/repository',
+ 'awes-io/repository',
+ 'green-fox-academy/git-lesson-repository',
+ 'thinkgem/repository',
+ 'darcyclarke/Repo.js',
+ 'repology/repology-updater',
+ 'Yet-Another-Series/Yet_Another_Algorithms_Repository',
+ 'OAI/OpenAPI-Specification',
+ 'touchgfx/touchgfx-open-repository',
+ 'technojam/Ultimate_Algorithms_Repository',
+ 'ioBroker/ioBroker.repositories',
+ 'philtabor/Youtube-Code-Repository',
+ 'dustinschultz/scf-config-repository',
+ 'RobThree/MongoRepository',
+ 'cnych/kubeapp',
+ 'grafana/grafana-plugin-repository',
+ 'dragonslayerx/Competitive-Programming-Repository',
+ 'nurkiewicz/spring-data-jdbc-repository',
+ 'magento-hackathon/composer-repository',
+ 'gamedilong/anes-repository',
+ 'collabH/repository',
+ 'kodi-czsk/repository',
+ 'sonatype/nexus-oss',
+ 'JustryDeng/PublicRepository',
+ 'GitIndonesia/awesome-indonesia-repo',
+ 'SharpRepository/SharpRepository',
+ 'jenkins-infra/repository-permissions-updater',
+ 'hadynz/repository.arabic.xbmc-addons',
+ 'Gexos/Hacking-Tools-Repository',
+ 'archlinuxcn/repo',
+ 'bosnadev/repository',
+ 'hassio-addons/repository',
+ 'sonatype/nexus-public',
+ 'andersao/l5-repository',
+ 'yiyang74262580/Repository',
+ 'matthewschrager/Repository',
+ 'ocaml/opam-repository',
+ 'sonatype/docker-nexus3',
+ 'puli/repository',
+ 'LuaDist/Repository',
+ 'OnsenUI/onsen.io',
+ 'AlexanderSharykin/RepositoryUtilities',
+ 'mzmine/mzmine2',
+ 'jackiekazil/data-wrangling',
+ 'haskell/hackage-server',
+ 'haskell/hackage-server',
+ 'mp2893/doctorai',
+ 'SwiftPackageIndex/PackageList',
+ 'openstreetmap/josm',
+ 'shihenw/convolutional-pose-machines-release',
+ 'ace-lectures/pattern-repository',
+ 'alanmcgovern/monotorrent',
+ 'PacktPublishing/Learning-PySpark',
+ 'traviscross/mtr',
+ 'deepmind/deepmind-research',
+ 'Sroy20/machine-learning-interview-questions',
+ 'x64dbg/docs',
+ 'thuehlinger/daemons',
+ 'fvwmorg/fvwm',
+ 'AlexanderSharykin/RepositoryUtilities',
+ 'OnsenUI/onsen.io',
+ 'grpc/grpc.io',
+ 'colcon/colcon-mixin-repository',
+ 'RIPE-NCC/whois',
+ 'glouppe/phd-thesis',
+ 'creative-computing-society/Hacktoberfest2020_CCS',
+ 'sous-chefs/nodejs',
+ 'Huawei/dockyard',
+ 'freeorion/freeorion',
+ 'sous-chefs/haproxy',
+ 'Rhombik/rhombik-object-repository',
+ 'vmware-tanzu/tgik',
+ 'ztrhgf/Powershell_CICD_repository',
+ 'jonatasbaldin/awesome-awesome-awesome',
+ 'nartc/nest-mean',
+ 'mateodelnorte/meta-npm',
+ 'reportportal/reportportal',
+ 'OpenScienceMOOC/Module-6-Open-Access-to-Research-Papers',
+ 'planetfederal/suite',
+ 'bitcoin-core/gui',
+ 'hibernate/hibernate-demos',
+ 'vrpn/vrpn',
+ 'chrisdee/Tools',
+ 'google/gitiles',
+ 'palominodb/PalominoDB-Public-Code-Repository',
+ 'djoos-cookbooks/newrelic',
+ 'miztiik/DevOps-Demos',
+ 'mp2893/med2vec',
+ 'components/jquery',
+ 'TRI-ML/packnet-sfm',
+ 'hiteshbpatel/Android_Blog_Projects',
+ 'vmware/open-vm-tools',
+ 'Flowpack/Flowpack.ElasticSearch.ContentRepositoryQueueIndexer',
+ 'arXivTimes/arXivTimes',
+ 'python-babel/babel',
+ 'gin-gonic/examples',
+ 'krazydanny/laravel-repository',
+ 'downthemall/downthemall-legacy',
+ 'ejwa/gitinspector',
+ 'CoreWCF/CoreWCF',
+ 'SOCI/soci',
+ 'foundeo/cfdocs',
+ 'sitemesh/sitemesh2',
+ 'foyzulkarim/GenericComponents',
+ 'OoliteProject/oolite',
+ 'fptudelft/FP101x-Content-2015',
+ 'host505/repository.host505',
+ 'razorpay/ifsc',
+ 'JoeyDeVries/LearnOpenGL',
+ 'danmunn/redmine_dmsf',
+ 'IseHayato/GitTestRepository',
+ 'winhu/MongoDB.Repository',
+ 'friendly-telegram/friendly-telegram',
+ 'asquarezone/AnsibleZone',
+ 'sous-chefs/nagios',
+ 'UNC-Libraries/Carolina-Digital-Repository',
+ 'sharpdx/SharpDX-Samples',
+ 'unitycontainer/unity',
+ 'Potential17/Hacktoberfest-2020',
+ 'libfann/fann',
+ 'foyzulkarim/GenericComponents',
+ 'VolodyaVechirko/RepositoryApp',
+ 'Wiznet/ioLibrary_Driver',
+ 'rg-engineering/ioBroker.heatingcontrol',
+ 'webanno/webanno',
+ 'sqlmapproject/udfhack',
+ 'mixu/gr',
+ 'akeneo/pim-community-dev',
+ 'jawi/ols',
+ 'nullism/clickingbad',
+ 'openpmix/openpmix',
+ 'brendt/settings-repository',
+ 'rhiever/Data-Analysis-and-Machine-Learning-Projects',
+ 'Brewtarget/brewtarget',
+ 'efreesen/active_repository',
+ 'disconnectme/disconnect-tracking-protection',
+ 'src-d/hercules',
+ 'ionic-team/ionic-bower',
+ 'flowable/flowable-examples',
+ 'georgy/nexus-npm-repository-plugin',
+ 'tier4/AutowareArchitectureProposal.iv',
+ 'xrootd/xrootd',
+ 'objcio/functional-swift',
+ 'mspnp/elasticsearch',
+ 'xinu-os/xinu',
+ 'fayland/dist-zilla-plugin-repository',
+ 'myopencart/ocStore',
+ 'Futureazoo/TextureRepository',
+ 'recalbox/recalbox-os',
+ 'objcode/v8',
+ 'chef-boneyard/database',
+ 'microsoft/moodle-repository_onenote',
+ 'w3c/csswg-test',
+ 'devit-tel/mongoose-repository',
+ 'QuickBlox/q-municate-ios',
+ 'CoskunKurtuldu/GenericRepositoryPattern',
+ 'chef-cookbooks/sudo',
+ 'tomgi/git_stats',
+ 'Finesse/web-fonts-repository',
+ 'supermamon/Reposi3',
+ 'tv42/gitosis',
+ 'duorg/Scripts',
+ 'fluent/fluentd-docs',
+ 'RocketChat/Docker.Official.Image',
+ 'PHPFusion/PHPFusion',
+ 'sous-chefs/tomcat',
+ 'TelerikAcademy/HTML',
+ 'sous-chefs/apt',
+ 'Harvard-IACS/2018-CS109A',
+ 'Yenthe666/Odoo_Samples',
+ 'boochtek/activerecord-repository',
+ 'sexym0nk3y/Laravel-5.3-Repository',
+ 'backmeupplz/voicy',
+ 'xroche/httrack',
+ 'Shougo/neosnippet-snippets',
+ 'lensfun/lensfun',
+ 'LGalaxiesPublicRelease/LGalaxies_PublicRepository',
+ 'dspinellis/awesome-msr',
+ 'rudrakshpathak/laravel-service-repository-pattern',
+ 'hacs/default',
+ 'chaotic-aur/infra',
+ 'DaveGut/DEPRECATED-TP-Link-SmartThings',
+ 'SkinsRestorer/SkinsRestorerX',
+ 'buddhi1980/mandelbulber2',
+ 'kubeflow/examples',
+ 'Azure/azure-policy',
+ 'exvim/main',
+ 'OWASP/owasp.github.io',
+ 'hraban/tomono',
+ 'ElmerCSC/elmerfem']
+
+    return [process_repo(repo) for repo in REPOS]
+
+
+def get_repo_data(cached=False):
+    '''
+    This function reads in repo data from github database and writes data to
+    a csv file if cached == False or if cached == True reads in repo df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('repo_data.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = pd.DataFrame(scrape_github_data())
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('repo_data.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('repo_data.csv', index_col=1)
+        
     return df
--- prepare.py
+++ prepare.py
@@ -5,6 +5,8 @@ import re
 from requests import get
 from bs4 import BeautifulSoup
 import acquire
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 import unicodedata
 import re
@@ -87,3 +89,174 @@ def make_prepped_columns(df, column):
     df['stemmed'] = df['lemmatized'].apply(lambda x: stem(x))
     df['lemmatized'] = df['lemmatized'].apply(lambda x: lemmatize(x))
     return df
+
+''' 
+Takes in readme contents and applies the make_prepped_columns() function. Drops altered readme 
+columns with the exception of the lemmatized one (as that's what I'll be working with) and the original.
+Also gets rid of rows containing null readme or null language columns. Drops rows if their respective language 
+appears less than twice (can't split it). Removes stopwords.
+'''
+
+def prep_repos(df):
+    
+    # Converts readme into a string
+
+    df['readme_contents'] = df['readme_contents'].apply(lambda x: str(x))
+
+    # Applying make_prepped_columns function
+
+    df = make_prepped_columns(df, 'readme_contents')
+
+    # Clears out text that is in foreign characters
+
+    df = df[df['readme_contents'].map(lambda x: x.isascii())]
+
+    # Drops unnecessary columns
+
+    df.drop(columns = ['Unnamed: 0', 'clean', 'stemmed'], inplace = True)
+
+    # Renames the lemmatized column to "readme_contents_clean"
+
+    df.rename(columns = {'lemmatized': 'readme_contents_clean'}, inplace = True)
+
+    # Removes rows with nan values in readme_contents_clean column
+
+    df = df[(~df['readme_contents_clean'].str.contains("nan")) & (df['readme_contents_clean'].str.len() > 20)]
+
+    # Drops any rows with null values in language column
+
+    df.dropna(how = 'any', inplace = True)
+
+    # Drops any row that's respective language only appears once in the data set (This prevents me from being able to split it)
+
+    df = df.groupby('language').filter(lambda x : len(x)>2)
+
+    # Resets the index to standard numerical
+
+    df = df.reset_index()
+
+    # A list of additional stopwords 
+
+    remove_words = ["'", '&#9;', 'repository', 'file', 'use', 'build', 'version', 'using']
+
+    # Applying the removal of stopwords
+
+    df['readme_contents_clean'] = df['readme_contents_clean'].apply(lambda x: remove_stopwords(x, extra_words = remove_words))
+    return df
+
+'''
+Adds a feature that searches the readme for mentions of a specific coding language and extracts it, then puts that language into the 
+"languages_in_readme" column. Also adds a feature for "readme_length"
+'''
+
+def add_language_dummies_and_length_feature(df):
+
+    # List of languages to pick up if found in the readme
+
+    languages = [' Python ', ' python ', ' java ', ' Java ', ' C++ ', ' c++ ', ' c# ', ' C# ' ' PHP ',  ' php ', ' Javascript ', 
+             'JavaScript', ' javascript ', ' Shell ', ' shell ', ' HTML ', ' html ', ' Ruby ', ' ruby ',
+             ' Jupyter Notebook', ' jupyter notebook', ' Typescript', ' TypeScript ', ' typescript ', ' Go ']
+
+    # Container for the found languages of all readme's
+
+    the_languages = []
+
+    # Iterating over each row
+
+    for index, row in df.iterrows():
+
+    # Container for found languages of each readme
+
+        holder = []
+
+    # Iterating over the list of languages
+        for word in languages:
+
+    # Setting if statement for when a word is found in the readme but not already in the "holder" container
+
+            if word in row['readme_contents'] and word not in holder:
+
+    # If a word in the list languages is found in the readme, put it in the "holder" container
+
+                holder.append(word.lower())
+
+    # Once all the languages have been extracted from a single readme, put that list into the "the_languages" container
+
+        the_languages.append(holder)
+
+    # Putting "the_languages" container into a pandas dataframe in a column titled "languages_in_readme"
+
+    df2 = pd.DataFrame({'languages_in_readme': the_languages})
+
+    # Normalizing the languages found in the readme by making each character lowercase after converting each element into a string
+
+    df2['languages_in_readme'] = df2['languages_in_readme'].apply(lambda x: str(x).lower())
+
+    # Removes list brackets from the now string
+
+    df2['languages_in_readme'] = df2['languages_in_readme'].apply(lambda x: x[1:-1])
+
+    # Puts a NaN value into empty values for "languages_in_readme"
+
+    df2 = df2.replace('', np.nan)
+
+    # Drops original (uncleaned) readme data from the original dataframe
+
+    df.drop(columns = ['readme_contents'], inplace = True)
+
+    # Creates a column in the original dataframe for length of the readme 
+
+    df['readme_length'] = df['readme_contents_clean'].apply(lambda x: len(x))
+
+    # Concatenates original and new dataframe together
+
+    df = pd.concat([df, df2], axis =1)
+
+    # Creates dummies for each language found to be in the readmes
+
+    df['has_python'] = df.languages_in_readme.str.contains('python')
+    df['has_php'] = df.languages_in_readme.str.contains('php')
+    df['has_html'] = df.languages_in_readme.str.contains('html')
+    df['has_typescript'] = df.languages_in_readme.str.contains('typescript')
+    df['has_ruby'] = df.languages_in_readme.str.contains('ruby')
+    df['has_shell'] = df.languages_in_readme.str.contains('shell')
+    df['has_c++'] = df.languages_in_readme.str.contains(r'c\+\+')
+    df['has_java'] = df.languages_in_readme.str.contains(r'java ')
+    df['has_javascript'] = df.languages_in_readme.str.contains('javascript')
+    df['has_go'] = df.languages_in_readme.str.contains('go')
+    df["has_python"] = df["has_python"].fillna(0)
+    df["has_python"] = df["has_python"].astype(int)
+    df['has_php'] = df['has_php'].fillna(0)
+    df['has_php'] = df['has_php'].astype(int)
+    df['has_html'] = df['has_html'].fillna(0)
+    df['has_html'] = df['has_html'].astype(int)
+    df['has_typescript'] = df['has_typescript'].fillna(0)
+    df['has_typescript'] = df['has_typescript'].astype(int)
+    df['has_ruby'] = df['has_ruby'].fillna(0)
+    df['has_ruby'] = df['has_ruby'].astype(int)
+    df['has_shell'] = df['has_shell'].fillna(0)
+    df['has_shell'] = df['has_shell'].astype(int)
+    df['has_c++'] = df['has_c++'].fillna(0)
+    df['has_c++'] = df['has_c++'].astype(int)
+    df['has_java'] = df['has_java'].fillna(0)
+    df['has_java'] = df['has_java'].astype(int)
+    df['has_javascript'] = df['has_javascript'].fillna(0)
+    df['has_javascript'] = df['has_javascript'].astype(int)
+    df['has_go'] = df['has_go'].fillna(0)
+    df['has_go'] = df['has_go'].astype(int)
+
+
+
+
+    return df
+
+'''
+Splits the data into train, validate, and test
+'''
+
+def split(df, stratify_by=None):
+    train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+    
+    train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train[stratify_by])
+    
+    return train, validate, test
